5ddc83e558bb8c5ac36d81c40e68bd5b7a4794f5
dazzlingstreak <dazzlingstreak@hotmail.com>
2017-02-28 14:45:50 +0800

    委托和事件的测试  ConsoleApp/Program

--- Dz.Core/ConsoleApp/Program.cs
+++ Dz.Core/ConsoleApp/Program.cs
@@ -14,9 +14,15 @@ namespace ConsoleApp
     {
         static void Main(string[] args)
         {
+            #region Redis
+
             //RedisProvider.StringSet("Dz.Redis", "ah yo22", TimeSpan.FromMinutes(1));
             //Console.WriteLine(RedisProvider.StringGet("Dz.Redis"));
             //Console.ReadLine();
+
+            #endregion
+
+            #region OSS
 
             //using (var stream = new FileStream(@"D:\DwProjects\23.jpg", FileMode.Open))
             //{
@@ -51,6 +57,43 @@ namespace ConsoleApp
             //OssHelper.DeleteObject("测试文件.jpg");
 
             //OssHelper.GetObject("测试文件.jpg", @"D:\DwProjects\hh.jpg");
+
+            #endregion
+
+            #region delegate/event
+
+            var radiant = new Radiant();
+            var dire = new Dire();
+            new Radiant.BPEventHandler((string s) => { }).Invoke("");//委托可以在外部单独调用，实例化
+
+            radiant.Ban_Event += new Radiant.BPEventHandler(LiveCommentating.CommentatingBanEnd); //解说员订阅天辉的Ban事件，天辉的Ban事件的类型是BPEventHandler的，它可以将与自己定义一样入参形式和返参形式的方法注册到事件上，这样事件发布时，那么事件上注册的各方法依次执行。表现为：发布事件->订阅方响应事件
+            radiant.Ban_Event += new Radiant.BPEventHandler(LiveCommentating.CommentatingBanAnalysis);
+            radiant.Ban_Event += new Radiant.BPEventHandler(Player.AfterBanEnd);//扩展一个选手的订阅，和解说员一样，可以注册自己的方法至事件上。设计模式：观察者模式
+            radiant.Ban_Event += new Radiant.BPEventHandler(Player.AnalysisBanEnd);
+
+            //注：使用事件的好处，而不是使用常规的IF判断 ：if(ban){ 解说员干什么，选手干什么}，用了if,那么天辉的Ban方法就会很复杂，如果又有了现场观众需要干什么，网络直播需要干什么，就要不停的扩展修改这个方法。
+            //使用事件的话，Ban方法不用修改，只需要将其他的情况注册到事件下，就可以了
+
+            //radiant.Pick_Event += new Radiant.BPEventHandler(LiveCommentating.CommentatingPickEnd);
+            //radiant.Pick_Event += new Radiant.BPEventHandler(LiveCommentating.CommentatingPickAnalysis);
+
+            //dire.Ban_Event += new Dire.BPEventHandler(LiveCommentating.CommentatingBanEnd);
+            //dire.Ban_Event += new Dire.BPEventHandler(LiveCommentating.CommentatingBanAnalysis);
+            //dire.Pick_Event += new Dire.BPEventHandler(LiveCommentating.CommentatingPickEnd);
+            //dire.Pick_Event += new Dire.BPEventHandler(LiveCommentating.CommentatingPickAnalysis);
+
+            radiant.Ban("德鲁伊");
+            //dire.Ban("小强");
+            //dire.Ban("小精灵");
+            //radiant.Ban("卡尔");
+            //radiant.Pick("大鱼人");
+            //dire.Pick("半人马酋长");
+            //dire.Pick("术士");
+            //radiant.Pick("小狗");
+
+            Console.ReadLine();
+
+            #endregion
         }
     }
 }
---
+++ Dz.Core/ConsoleApp/委托和事件/DelegateEvent_Test.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp
+{
+    /// <summary>
+    /// Dota2 -天辉
+    /// </summary>
+    class Radiant
+    {
+        internal delegate void BPEventHandler(string hero);
+        internal event BPEventHandler Ban_Event;
+        internal event BPEventHandler Pick_Event;
+
+        public void Ban(string hero)
+        {
+            Console.WriteLine($"天辉Ban {hero} ...");
+            Ban_Event?.Invoke(hero);
+        }
+
+        public void Pick(string hero)
+        {
+            Console.WriteLine($"天辉Pick {hero} ...");
+            Pick_Event?.Invoke(hero);
+        }
+    }
+
+    /// <summary>
+    /// Dota2 - 夜魇
+    /// </summary>
+    class Dire
+    {
+        internal delegate void BPEventHandler(string hero);
+        internal event BPEventHandler Ban_Event;
+        internal event BPEventHandler Pick_Event;
+
+        public void Ban(string hero)
+        {
+            Console.WriteLine($"夜魇Ban {hero} ...");
+            Ban_Event?.Invoke(hero);
+        }
+
+        public void Pick(string hero)
+        {
+            Console.WriteLine($"夜魇Pick {hero} ...");
+            Pick_Event?.Invoke(hero);
+        }
+    }
+
+    /// <summary>
+    /// 现场解说
+    /// </summary>
+    class LiveCommentating
+    {
+        public static void CommentatingBanEnd(string hero)
+        {
+            Console.WriteLine($"解说员说：Ban了{hero},这个英雄...");
+        }
+
+        public static void CommentatingBanAnalysis(string hero)
+        {
+            Console.WriteLine($"解说员说：Ban了{hero},分析下当前局势...");
+        }
+
+        public static void CommentatingPickEnd(string hero)
+        {
+            Console.WriteLine($"解说员说：Pick了{hero},这个英雄...");
+        }
+
+        public static void CommentatingPickAnalysis(string hero)
+        {
+            Console.WriteLine($"解说员说：Pick了{hero}，分析下当前局势...");
+        }
+    }
+
+    /// <summary>
+    /// 比赛选手
+    /// </summary>
+    class Player
+    {
+        public static void AfterBanEnd(string hero)
+        {
+            Console.WriteLine($"选手想：Ban了{hero},我得想想比赛套路...");
+        }
+
+        public static void AnalysisBanEnd(string hero)
+        {
+            Console.WriteLine($"选手想：Ban了{hero},分析了下目前形势...");
+        }
+
+        public static void AfterPickEnd(string hero)
+        {
+            Console.WriteLine($"选手想：Pick了{hero}，这个英雄谁去使用，走的是哪路...");
+        }
+
+        public static void AnalyPickEnd(string hero)
+        {
+            Console.WriteLine($"选手想：Pick了{hero}，想下这个英雄害怕什么，接下去要不要BAN掉什么...");
+        }
+    }
+
+}
